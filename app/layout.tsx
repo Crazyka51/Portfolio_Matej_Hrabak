a27ac85592abfed546d47675c634e2c71330ba21
Crazyka51 <145287796+Crazyka51@users.noreply.github.com>
2025-05-16 05:15:40 +0200

    insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ToastProvider } from "./context/toast-context"
 import GridBackground from "./components/grid-background"
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin", "latin-ext"] })
 
 export const metadata: Metadata = {
